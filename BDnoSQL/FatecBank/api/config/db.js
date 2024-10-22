e2576fd26d805b22505906732972e30f40637239
Zan-Kir <murilo4santos@hotmail.com>
2024-10-21 22:12:04 -0300

    BDnoSQL 21-10

---
+++ BDnoSQL/FatecBank/api/config/db.js
@@ -0,0 +1,16 @@
+import {MongoClient} from 'mongodb';
+import {config} from 'dotenv';
+config()
+
+const {MONGODB_URI, MONGODB_DB} = process.env
+
+let client;
+async function connectToDatabase() {
+    if(!client) {
+        client = new MongoClient(MONGODB_URI);
+        await client.connect();
+        console.log('🌿 Conectado ao MongoDB 🌿');
+    }
+    return client;
+}
+export {connectToDatabase, MONGODB_DB};
